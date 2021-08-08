8269a03901a36ee306f43e8da47b8b566bb00f03
cavadsalman <cavad2013@gmail.com>
2021-08-08 18:16:18 +0400

    table init

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,51 +6,58 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { Checkbox } from '@material-ui/core';
+import classes from './Table.module.scss';
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
-});
+
 
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Fazil Əmirli', 'Kasmetologiya', 'Tale Əliyev', '0918454356', '08.08.2021'),
+    createData('Aydan Əliyevə', '-', 'Defne Aras', '0917354356', '-'),
+    createData('Nihad Həsənli', 'Kardiologiya', 'Defne Aras', '0912344356', '12.08.2021'),
+    createData('Həsən Ələkbərli', '-', 'Mustafa Söylemezo', '0918458766', '09.08.2021'),
+    createData('Hilal Yılmaz', 'Cərrahiyyə', '-', '0918409856', '22.08.2021'),
+    createData('Fəhmin Şamilli', 'Stomotologiya', 'Rəşad Əlivev', '0913544356', '25.08.2021'),
 ];
 
 function BasicTable() {
-    const classes = useStyles();
 
     return (
         <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
+            <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Adı</TableCell>
+                        <TableCell>Şöbə</TableCell>
+                        <TableCell>Həkim</TableCell>
+                        <TableCell>Nömrə</TableCell>
+                        <TableCell>Tarix</TableCell>
+                        <TableCell>Seç</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
                         <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
+                            <TableCell>{row.name}</TableCell>
+                            <TableCell>{row.calories}</TableCell>
+                            <TableCell>{row.fat}</TableCell>
+                            <TableCell>{row.carbs}</TableCell>
+                            <TableCell>{row.protein}</TableCell>
+                            <TableCell className={classes.CheckboxCol}>
+                                <Checkbox
+                                    checked={true}
+                                    onChange={()=>{}}
+                                    color="primary"
+                                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                                    classes={{
+                                        root: classes.CheckBox
+                                    }}
+                                />
                             </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
